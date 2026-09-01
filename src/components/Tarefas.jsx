9daff4064fe312ff91a4698342a0d6693ac1a423
Giovanna <giscalzone@gmail.com>
2026-09-01 09:47:10 -0300

    feat: Funcionalidades de tarefas

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-
+import '../css/estilo.css'
 const Tarefas = () => {
     // HOOK useState
     const [tarefas, setTarefas] = useState(() => {
@@ -16,14 +16,50 @@ const Tarefas = () => {
     }, [tarefas])
 
     // Função para adicionar tarefa
-    const AdicionarTarefa = () => {
-        setTarefas("teste", "1234")
+    const AdicionarTarefa = (e) => {
+        // Previne que a página se recarregue
+        e.preventDefault();
+        // valida o campo se for vazio
+        if (!campo.trim()) return;
+
+        const novaTarefa = {
+            id: Date.now(),
+            text: campo,
+        };
+        setTarefas([...tarefas, novaTarefa])
+        setCampo('');
     }
 
-    return (
-        <>
+    // Função para remover tarefa
+    const RemoverTarefa = (id) => {
+        const apagarTarefa = tarefas.filter((tarefa) => tarefa.id !== id);
+        setTarefas(apagarTarefa);
+    }
 
-        </>
+
+
+    return (
+        <div className="todo-container"> 
+            <h1>Minha Lista de Tarefas</h1>
+            <form onSubmit={AdicionarTarefa}>
+                <input type="text"
+                    value={campo}
+                    onChange={(e) => { setCampo(e.target.value) }}
+                    placeholder="Digite uma tarefa"
+                />
+                <button type="submit">Adicionar</button>
+            </form>
+            <ul>
+                {tarefas.map((tarefa) => (
+                    <li key={tarefa.id}>
+                        <span>{tarefa.text}</span>
+                        <button onClick={() => RemoverTarefa(tarefa.id)}>Excluir</button>
+                    </li>
+                ))}
+            </ul>
+            {/* compara senão tiver tarefas deixar a nenhuma tarefa salva */}
+            {tarefas.length === 0 && <p>Nenhuma tarefa salva</p>}
+        </div>
     )
 }
 
